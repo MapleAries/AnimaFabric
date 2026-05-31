a59b34d11739b461603aff12118d1ac77eb28a16
Maple <timemapleling@gmail.com>
2026-05-31 17:08:32 +0800

    修复假玩家连接 side 验证
    - 重写 getReceiving() 返回 SERVERBOUND，使 placeNewPlayer 能正常设置监听器
    - 使用正确的 ChannelFutureListener 类型

--- src/main/kotlin/com/maple/entity/FakeClientConnection.kt
+++ src/main/kotlin/com/maple/entity/FakeClientConnection.kt
@@ -1,5 +1,6 @@
 package com.maple.entity
 
+import io.netty.channel.ChannelFutureListener
 import net.minecraft.network.Connection
 import net.minecraft.network.protocol.Packet
 import net.minecraft.network.protocol.PacketFlow
@@ -7,11 +8,25 @@ import net.minecraft.server.MinecraftServer
 
 /**
  * 假玩家的网络连接。
- * 将数据包广播给所有真实客户端，使假玩家在客户端可见。
+ * 重写 getReceiving() 返回 SERVERBOUND，使 placeNewPlayer() 能正常设置监听器。
+ * send() 将数据包广播给所有真实客户端。
  */
 class FakeClientConnection(private val server: MinecraftServer) : Connection(PacketFlow.CLIENTBOUND) {
 
+    /**
+     * 关键：返回 SERVERBOUND 使 setupInboundProtocol() 不会抛出异常。
+     */
+    override fun getReceiving(): PacketFlow = PacketFlow.SERVERBOUND
+
     override fun send(packet: Packet<*>) {
+        broadcastPacket(packet)
+    }
+
+    override fun send(packet: Packet<*>, listener: ChannelFutureListener?) {
+        broadcastPacket(packet)
+    }
+
+    override fun send(packet: Packet<*>, listener: ChannelFutureListener?, flush: Boolean) {
         broadcastPacket(packet)
     }
 
